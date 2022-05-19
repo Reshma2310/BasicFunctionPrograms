b2bc3b4694b18f8a13d9d576d9892fd738a912de
Reshma2310 <dreshma253@gmail.com>
2022-05-19 15:20:30 +0500

    [Reshma]- Flip Coin Percentage Program

---
+++ BasicFunctionPrograms/BasicFunctionPrograms/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+BasicFunctionPrograms.FlipCoinPercentage output = new BasicFunctionPrograms.FlipCoinPercentage();
+output.FlipCoin();
