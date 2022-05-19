df2cead352c1e131db6b1dacee4ac600349a6738
Reshma2310 <dreshma253@gmail.com>
2022-05-19 17:52:17 +0500

    [Reshma]- Factors of given Number

--- BasicFunctionPrograms/BasicFunctionPrograms/Program.cs
+++ BasicFunctionPrograms/BasicFunctionPrograms/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("1. Flip Coin\n2. Leap Year\n3. Power of 2\n4. Harmonic Value");
+Console.WriteLine("1. Flip Coin\n2. Leap Year\n3. Power of 2\n4. Harmonic Value\n5. Factors");
 int choice = Convert.ToInt32(Console.ReadLine());
 switch ( choice )
 {
@@ -18,6 +18,10 @@ switch ( choice )
     case 4:
         BasicFunctionPrograms.HarmonicNumber harmonic = new BasicFunctionPrograms.HarmonicNumber();
         harmonic.HarmonicValue();
+        break;
+    case 5:
+        BasicFunctionPrograms.Factors factors = new BasicFunctionPrograms.Factors();
+        factors.Factor();
         break;
     default: Console.WriteLine("Invalid Input");
         break;
