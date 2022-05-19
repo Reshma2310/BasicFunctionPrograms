6e0939d49e2c7c843fb2a0e214148fccc1793c28
Reshma2310 <dreshma253@gmail.com>
2022-05-19 15:46:13 +0500

    [Reshma] - Entered Number is Leap Year or Not

--- BasicFunctionPrograms/BasicFunctionPrograms/Program.cs
+++ BasicFunctionPrograms/BasicFunctionPrograms/Program.cs
@@ -1,4 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-BasicFunctionPrograms.FlipCoinPercentage output = new BasicFunctionPrograms.FlipCoinPercentage();
-output.FlipCoin();
+Console.WriteLine("1. Flip Coin\n2. Leap Year");
+int choice = Convert.ToInt32(Console.ReadLine());
+switch ( choice )
+{
+    case 1:
+        BasicFunctionPrograms.FlipCoinPercentage output = new BasicFunctionPrograms.FlipCoinPercentage();
+        output.FlipCoin();
+        break;
+    case 2:
+        BasicFunctionPrograms.LeapYear year = new BasicFunctionPrograms.LeapYear();
+        year.LeapYearProg();
+        break;
+    default: Console.WriteLine("Invalid Input");
+        break;
+}
